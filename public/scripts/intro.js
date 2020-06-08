afcf587148ca4fd6c4ea0bc67baf63392c4385e6
Rodrigues-cloud <uesleyrs2020@outlook.com>
2020-06-08 13:44:30 -0300

    Adicionando o projeto completo de Ecoleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,58 @@
+// document.write("hello")
+
+//let myvar
+//mayvar = "hello"
+//const myconst= "heeee"
+
+//document.write(mayvar + myconst)
+
+//objetos
+
+// const pessoa = {
+//   altura: "1,80",
+//   idade: 24,
+//   casado: true,
+//   correr(){
+    
+//       document.write("executar uma logica de correr!")
+//   }
+// }
+
+// pessoa.correr()
+
+//array - vetores
+
+// const colecaoDeBolinhas = ["blue","red", 1, {name:"Uesley"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+//funcoes - funcionalidades - atalhos - reuso de codigo
+
+// function nomes(name) {
+  
+//   document.write(name)
+// }
+
+// nomes("Uesley")
+// nomes("Pollyanna")
+// nomes("Giovanna")
+
+
+//condicoes
+
+// const notaFinal = 7
+// if( notaFinal < 5) {
+//   //caminho 1
+//   document.write("Reprovado")
+// } else {
+//   //caminho2
+//   document.write("Aprovado")
+  
+// }
+
+
+// //loops
+
+// for( i=0; i<=10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
